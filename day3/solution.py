bf6f97b9759c578403c33657c36df6d4e02d4ff0
Ivan Toriya <ivantoriya@gmail.com>
2025-12-03 17:32:28 +0100

    add day3 part1

---
+++ day3/solution.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+input = Path("day3/input.txt").read_text()
+
+
+total = 0
+
+for bank in input.splitlines():
+    combinations = set()
+    previous = 0
+    for idx, battery in enumerate(bank):
+        if int(battery) <= previous:
+            continue
+        combinations.update((int(battery + b) for b in bank[idx + 1 :]))
+        previous = int(battery)
+    total += max(combinations)
+
+
+print("Part 1: ", total)
