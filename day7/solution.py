f4dc4bd14d28f2441c359bfdd28fbb901aa36c02
Ivan Toriya <ivantoriya@gmail.com>
2025-12-07 16:49:00 +0100

    add day7 part1

---
+++ day7/solution.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+
+
+def parse_input(filename: str):
+    inp = (Path(__file__).parent / filename).read_text()
+    return [list(line) for line in inp.splitlines()]
+
+
+# === Part 1 ===
+
+
+def part1(manifold: list[list[str]]):
+    beams = [{manifold[0].index("S")}]
+    n_splits = 0
+
+    for i in range(0, len(manifold) - 1):
+        beams.append(set())  # next fold
+
+        for b in beams[i]:
+            match manifold[i + 1][b]:
+                case ".":
+                    beams[i + 1].add(b)
+                case "^":
+                    beams[i + 1].add(b - 1)
+                    beams[i + 1].add(b + 1)
+                    n_splits += 1
+                case _:
+                    raise NotImplementedError("Are you sure you're traveling through tachyon manifolds?")
+
+    return n_splits
+
+
+if __name__ == "__main__":
+    manifold = parse_input("input.txt")
+    print("Part 1: ", part1(manifold))
